0a2ba0a66458e2b7f89b80abed4a1451f9f1b06f
Madff386 <69940651+Madff386@users.noreply.github.com>
2020-12-04 14:09:15 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pkelib", 
-    version="0.0.2",
+    version="1.0.0",
     author="Mdaff169",
     author_email="dehersbach@gmail.com",
     description="PKE library",
